249968939d2cf1117025bb51b7ee1667653db664
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-12-19 09:28:32 +0530

    Taking input using input function and type cast them.

---
+++ Python basics/input.py
@@ -0,0 +1,8 @@
+# input alway take string value
+# so we use here TYPE CASTING to convert it.
+
+a = int(input("Enter First no: "));
+b = int(input("Enter Second no: "));
+print("The sum of two number is: ",a+b);
+c = int(input("Enter val: "));
+print(type(c),c);

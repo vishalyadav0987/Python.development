3b43de0107f399dd0c873cdd2cdf05af3d27947d
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-12-20 09:14:43 +0530

    Tuples basics immutable

---
+++ Python basics/List and Tuples/Tuples.py
@@ -0,0 +1,23 @@
+# TUPLE Immutable in python
+# it hold multiple data type at a time
+
+tup = (1,2,3,4,5);
+print(tup);
+print(type(tup));
+
+# tup[0] = 5; not possible in tupple
+
+tup1 = (1); # considersed as int val
+print(tup1)
+print(type(tup1));
+
+tup2 = (1,); # considersed as tupple
+print(tup2)
+print(type(tup2));
+
+tup3 = (); # emty as tupple
+print(tup3)
+print(type(tup3));
+
+# All method possibles
+

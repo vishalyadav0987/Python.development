09d13645a13abbdf322e614f7e99487ff8f1d51c
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-12-28 20:31:05 +0530

    loops

---
+++ Python basics/Loops/for.py
@@ -0,0 +1,22 @@
+# nums = [10,11,12,13,14,12,89,56];
+# for i in nums:
+#     print(i);
+
+
+# nums = 10;
+# num = 2;
+# for i in range(10):
+#     print(num ,"*",i ,"=",nums*num );
+
+
+# for i in range(10,30): # start stop
+#     print(num ,"*",i ,"=",nums*num );
+
+# for i in range(10,30,2): # start stop skip
+#     print(num ,"*",i ,"=",nums*num );
+
+for i in range(10,1,-1): # start stop skip
+    print(i)
+
+for i in range(12):
+    pass
---
+++ Python basics/Loops/while.py
@@ -0,0 +1,20 @@
+i=0;
+while i<=5:
+    print(i)
+    i+=1;
+
+print("Loop finished");
+
+
+while i>=0:
+    print(i)
+    i-=1;
+
+print("Loop finished");
+
+i=0
+print("Print Table");
+tableOf  = int(input("Enter a number: "));
+while i<10:
+    print(tableOf , "*" , i+1 , "=" , tableOf*(i+1))
+    i+=1;
